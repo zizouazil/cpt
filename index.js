35477cca8df78f6c9ae0add8b32e9e499ffc494b
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-17 20:27:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,31 @@ if (!channel) return;
 channel.send({embed : embed});
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(" ✅    تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+
+-${message.guild.name}  Link
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 client.on("message", message => {
               var args = message.content.substring(prefix.length).split(" ");

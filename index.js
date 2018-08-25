2efa6dde538eca798090250305264617ea29134d
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-25 15:10:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '+';
+const prefix = '!';
 
 client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');

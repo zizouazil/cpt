10e8fa06da52e83268437be6d1bd0808abbb1065
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-25 15:35:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1434,31 +1434,6 @@ client.on("ready", () => {
         })
     })
 })
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'winter');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "! Winter")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` ♥ **Invited By** : ${Invite.inviter} ♥ `)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
+
 
 client.login(process.env.BOT_TOKEN);

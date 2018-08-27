52b8ffb42a0caca9c629d0ae3d78f599cc732cc3
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-27 13:14:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const a = new Discord.client();
+const a = new Discord.Client();
 const ytdl = require('ytdl-core');
 const request = require('request');
 const fs = require('fs');

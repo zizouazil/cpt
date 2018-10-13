5c927fcd07690dab04dc4182571bcda5f71d6c58
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-10-13 19:00:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '!';
+const prefix = 'R';
 
 client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('483018550051864577');
